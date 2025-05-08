886672568878be11a183202f8481c827f94aac18
stabmeldys <lukas.les93@gmail.com>
2025-05-08 18:24:38 +0300

    run adjusted to 32 bits programs

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -2,6 +2,7 @@
 
 program_name=$1
 
-as "src/$program_name".s -o "object_files/$program_name.o"
-ld "object_files/$program_name.o" -o "executables/$program_name"
+as --32 "src/$program_name".s -o "object_files/$program_name.o"
+ld -m elf_i386 "object_files/$program_name.o" -o "executables/$program_name"
 ./executables/"$program_name"
+echo $?
