2ac33cd8980eebd7ee186ff058413efdbc204ccf
stabmeldys <lukas.les93@gmail.com>
2025-05-08 18:25:15 +0300

    power: handles if power is zero

--- src/power.s
+++ src/power.s
@@ -1,6 +1,6 @@
 #PURPOSE: Program to illustrate how functions work
 # This program will compute the value of
-# 2^3 + 5^2
+# 2^2 + 3^0
 #
 #Everything in the main program is stored in registers,
 #so the data section doesn’t have anything.
@@ -14,14 +14,14 @@
 .section .text
 .globl _start
 _start:
-	pushl $3 #push second argument
+	pushl $2 #push second argument
 	pushl $2 #push first argument
 	call power #call the function
 	addl $8, %esp #move the stack pointer back
 	pushl %eax #save the first answer before
 	#calling the next function
-	pushl $2 #push second argument
-	pushl $5 #push first argument
+	pushl $0 #push second argument
+	pushl $3 #push first argument
 	call power #call the function
 	addl $8, %esp #move the stack pointer back
 	popl %ebx #The second answer is already
@@ -44,8 +44,6 @@ _start:
 #
 #OUTPUT: Will give the result as a return value
 #
-#NOTES: The power must be 1 or greater
-#
 #VARIABLES:
 # %ebx - holds the base number
 # %ecx - holds the power
@@ -59,18 +57,21 @@ power:
 	pushl %ebp #save old base pointer
 	movl %esp, %ebp #make stack pointer the base pointer
 	subl $4, %esp #get room for our local storage
-	movl 8(%ebp), %ebx #put first argument in %eax
+	movl 8(%ebp), %ebx #put first argument in %ebx
 	movl 12(%ebp), %ecx #put second argument in %ecx
 	movl %ebx, -4(%ebp) #store current result
 power_loop_start:
+	cmpl $0, %ecx
+	je power_zero
 	cmpl $1, %ecx #if the power is 1, we are done
 	je end_power
 	movl -4(%ebp), %eax #move the current result into %eax
-	imull %ebx, %eax #multiply the current result by
-	#the base number
+	imull %ebx, %eax #multiply the current result by the base number
 	movl %eax, -4(%ebp) #store the current result
 	decl %ecx #decrease the power
 	jmp power_loop_start #run for the next power
+power_zero:
+	movl $1, -4(%ebp)
 end_power:
 	movl -4(%ebp), %eax #return value goes in %eax
 	movl %ebp, %esp #restore the stack pointer
