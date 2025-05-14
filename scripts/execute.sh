bb952eff253067d7b6b37674137fcdc7d1ae08a7
stabmeldys <lukas.les93@gmail.com>
2025-05-14 20:14:30 +0300

    added option to not execute program

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 program_name=$1
+run_after=${2:-false}
 
-as --32 "src/$program_name".s -o "object_files/$program_name.o"
+as --32 -g "src/$program_name".s -o "object_files/$program_name.o"
 ld -m elf_i386 "object_files/$program_name.o" -o "executables/$program_name"
-./executables/"$program_name"
-echo $?
+
+if [ "$run_after" = true ]; then
+	./executables/"$program_name"
+	echo $?
+fi
+
